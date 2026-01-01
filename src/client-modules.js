3cc3b73870ce0289ab06ed1e4d71e71b253529f0
Shuaibali0786 <160752334+Shuaibali0786@users.noreply.github.com>
2026-01-01 16:33:49 +0500

    Revert "1 translate search"

--- src/client-modules.js
+++
@@ -1,9 +0,0 @@
-/**
- * Client modules for Docusaurus
- *
- * This file registers the root component for translation and search functionality
- */
-
-import Root from './components/DocusaurusIntegration/Root';
-
-export { Root };
